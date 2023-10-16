e47d617d49af0d48ec3da46574ed9150f2d62b72
uv777bk <147540311+uv777bk@users.noreply.github.com>
2023-10-16 09:11:31 -0400

    Add files via upload

--- keyboards/monsgeek/m5/m5.c
+++ keyboards/monsgeek/m5/m5.c
@@ -158,6 +158,8 @@ bool led_update_kb(led_t led_state) {
     bool res = led_update_user(led_state);
     if (res) {
         writePin(LED_WIN_LOCK_PIN, keymap_config.no_gui);
+        writePin(A15, led_state.num_lock);
+        writePin(C10, led_state.caps_lock);
     }
     return res;
 }
